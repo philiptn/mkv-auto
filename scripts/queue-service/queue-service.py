f2b277dc542186e7e4a4fb39faa880794a3cbed6
philiptn <philipt@uia.no>
2025-01-15 23:18:58 +0100

    Simplified queue service to simply copy to mkv-auto input folder

--- scripts/queue-service/queue-service.py
+++ scripts/queue-service/queue-service.py
@@ -34,12 +34,10 @@ def convert_path(win_path):
     return win_path, is_directory
 
 
-def process_file(file_path, output_folder):
+def process_file(file_path):
     mkv_auto_folder_path = '/media/philip/nvme/mkv-auto/'
-    ready_for_final_processing_path = '/media/share/mkv-auto-queue/files/'
 
     lock_file_path = file_path + '.lock'
-    ready_for_nvenc_folder_path = None
 
     # Check for the existence of the lock file
     if os.path.exists(lock_file_path):
@@ -64,53 +62,9 @@ def process_file(file_path, output_folder):
 
     # If tag is 'Plex', copy files and process using mkv-auto
     if tag == 'Plex':
-        if not is_directory:
-            # If the path is directly to a file, copy the file to mkv-auto input folder
-            try:
-                subprocess.run(["cp", linux_path, f'{mkv_auto_folder_path}input/'])
-            except Exception as e:
-                print(f"\n[SERVICE] An error occurred while executing the command: {e}")
-
-            command = ["venv/bin/python3", "-u", "mkv-auto.py", "--output_folder",
-                       output_folder, "--silent", "--move"]
-        else:
-            command = ["venv/bin/python3", "-u", "mkv-auto.py", "--output_folder",
-                       output_folder, "--silent", "--input_folder", linux_path]
-
+        # If the path is directly to a file, copy the file to mkv-auto input folder
         try:
-            subprocess.run(command, cwd=mkv_auto_folder_path)
-        except Exception as e:
-            print(f"\n[SERVICE] An error occurred while executing the command: {e}")
-            os.remove(lock_file_path)  # Remove the lock file
-            return
-
-    # If tag contains 'NVEnc', copy files to queue path
-    elif "NVEnc" in tag:
-        if 'NVEnc18' in tag:
-            ready_for_nvenc_folder_path = '/media/share/mkv-auto-queue/nvenc_queue/quality_18'
-        elif 'NVEnc30' in tag:
-            ready_for_nvenc_folder_path = '/media/share/mkv-auto-queue/nvenc_queue/quality_30'
-
-        command = ["cp", "-r", linux_path, ready_for_nvenc_folder_path]
-
-        # Execute the command
-        try:
-            subprocess.run(command, cwd=mkv_auto_folder_path)
-        except Exception as e:
-            print(f"\n[SERVICE] An error occurred while executing the command: {e}")
-            os.remove(lock_file_path)  # Remove the lock file
-            return
-
-    # If tag is 'Ready', process using mkv-auto with no temp-copy
-    elif tag == 'Ready':
-        # Command template
-        command = ["venv/bin/python3", "-u", "mkv-auto.py", "--output_folder",
-                   output_folder, "--silent", "--move", "--input_folder",
-                   ready_for_final_processing_path]
-
-        # Execute the command
-        try:
-            subprocess.run(command, cwd=mkv_auto_folder_path)
+            subprocess.run(["cp", linux_path, f'{mkv_auto_folder_path}input/'])
         except Exception as e:
             print(f"\n[SERVICE] An error occurred while executing the command: {e}")
             os.remove(lock_file_path)  # Remove the lock file
@@ -135,15 +89,13 @@ def main():
         description="A service for mkv-auto that can parse input paths from a queue text file")
     parser.add_argument("--file_path", dest="file_path", type=str,
                         help="The path to the text file containing the input paths")
-    parser.add_argument("--output_folder", dest="output_folder", type=str,
-                        help="The output folder path used by mkv-auto to save its files")
     args = parser.parse_args()
 
     if not os.path.exists(args.file_path):
         print(f"\n[SERVICE] File {args.file_path} not found!")
         return
 
-    process_file(args.file_path, args.output_folder)
+    process_file(args.file_path)
 
 
 if __name__ == "__main__":
