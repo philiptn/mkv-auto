05d62972b11c845d4ae5dddb55a3b1fb4d8af1d3
philiptn <philipt@uia.no>
2025-12-27 22:50:36 +0100

    Fix version number

--- mkv-auto.py
+++ mkv-auto.py
@@ -12,7 +12,7 @@ from modules.misc import *
 from modules.logger import *
 from modules.media_encoder import *
 
-mkv_auto_version = 'DEV-2025-12'
+mkv_auto_version = 'v3.1.2'
 
 
 def mkv_auto(args):
